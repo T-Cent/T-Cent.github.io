b636ede9f1085f28c4702bd89c212a748635b38a
Taraash <taraashmittal@gmail.com>
2024-08-28 19:57:47 +0530

    backup

---
+++ script.js
@@ -0,0 +1,8 @@
+// const line = document.createElement("div");
+
+// line.className("grid");
+// line.style.marginLeft = "60px";
+// line.style.height = "100px";
+// line.style.width = "1px";
+// line.style.backgroundColor = "red";
+// document.body.appendChild(line);
